71da8d65de1d7f2807132407590632cbb0ee7edd
krampenschiesser <krampenschiesser@gmail.com>
2019-10-10 08:06:18 -0400

    initial

---
+++ playground/src/lib.rs
@@ -0,0 +1,11 @@
+#[derive(Default,Debug)]
+pub struct DomElement {
+    children: Vec<Box<DomElement>>
+}
+
+pub trait Component {
+    type GlobalState;
+    type LocalState;
+    fn filter_state(state: &Self::GlobalState) -> &Self::LocalState;
+    fn render() -> DomElement;
+}
---
+++ playground/src/main.rs
@@ -0,0 +1,59 @@
+use crate::lib::{Component, DomElement};
+
+mod lib;
+
+#[derive(Default, Debug)]
+struct ModuleState {
+    child1: ChildState1,
+    child2: ChildState2,
+}
+
+#[derive(Default, Debug)]
+struct ChildState1 {
+    value: String,
+}
+
+#[derive(Default, Debug)]
+struct ChildState2 {
+    value: u32,
+}
+
+#[derive(Default)]
+struct Component1 {}
+
+impl Component for Component1 {
+    type GlobalState = ModuleState;
+    type LocalState = ChildState1;
+
+    fn filter_state(state: &Self::GlobalState) -> &Self::LocalState {
+        &state.child1
+    }
+
+    fn render() -> DomElement {
+        DomElement::default()
+    }
+}
+
+#[derive(Default)]
+struct Component2 {}
+
+impl Component for Component2 {
+    type GlobalState = ModuleState;
+    type LocalState = ChildState2;
+
+    fn filter_state(state: &Self::GlobalState) -> &Self::LocalState {
+        &state.child2
+    }
+
+    fn render() -> DomElement {
+        DomElement::default()
+    }
+}
+
+fn main() {
+    let state = ModuleState::default();
+    let component1 = Component1::default();
+    let component2 = Component2::default();
+
+    println!("Hello, world!");
+}
